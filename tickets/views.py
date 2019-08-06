95315e0fb88a237b25aa11e391b9a78cf68db6b9
W <whobler@gmail.com>
2019-08-06 11:53:44 +0200

    use JsonResponse, beautify views

--- tickets/views.py
+++ tickets/views.py
@@ -1,22 +1,45 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.core import serializers
 
 from .payment_adapter import PaymentGateway, CardError, PaymentError, CurrencyError
 from .models import Events, Seats, Reservations
 
 
+NO_EVENT_FOUND_MESSAGE = "No event found."
+NO_TICKETS_FOR_EVENT_MESSAGE = "No available tickets found for this event."
+NO_TICKET_FOUND_MESSAGE = 'One or more tickets do not exist'
+TICKET_ALREADY_RESERVED_MESSAGE = 'One or more tickets already reserved.'
+
+
 class EventView(APIView):
     def get(self, request, event_id):
-        event = Events.objects.filter(pk=event_id)
-        return Response(serializers.serialize('json', event))
+        event = Events.objects.filter(pk=event_id).first()
+        if event:
+            data = {
+                'event_id': event.id,
+                'event_name': event.name,
+                'date_time': event.datetime
+            }
+        else:
+            data = {'info': NO_EVENT_FOUND_MESSAGE}
+        return JsonResponse(data)
 
 
 class AvailableTicketsView(APIView):
     def get(self, request, event_id):
-        available_tickets = Seats.objects.filter(reservation=None)
-        return Response(serializers.serialize('json', available_tickets))
+        event = Events.objects.filter(pk=event_id).first()
+        if event:
+            available_tickets = Seats.objects.filter(reservation=None, event=event)
+            if available_tickets:
+                data = {
+                    'available_tickets': [{'ticket_id': s.id, 'ticket_type': s.type} for s in available_tickets]
+                }
+            else:
+                data = {'info': NO_TICKETS_FOR_EVENT_MESSAGE}
+        else:
+            data = {'info': NO_EVENT_FOUND_MESSAGE}
+        return JsonResponse(data)
 
 
 class ReserveTicketView(APIView):
@@ -26,12 +49,14 @@ class ReserveTicketView(APIView):
         new_reservation.save()
         for seat_id in seat_ids.split(','):
             current_seat = Seats.objects.get(id=seat_id)
+            if not current_seat:
+                return JsonResponse({'error': NO_TICKET_FOUND_MESSAGE})
             if current_seat.reservation is None:
                 current_seat.reservation = new_reservation
                 current_seat.save()
             else:
-                return Response("Ticket(s) already reserved")
-        return Response(new_reservation.pk)
+                return JsonResponse({'error': TICKET_ALREADY_RESERVED_MESSAGE})
+        return JsonResponse({'new_reservation': new_reservation.pk})
 
 
 class PayView(APIView):
